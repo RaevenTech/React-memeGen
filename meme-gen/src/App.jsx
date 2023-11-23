cea9034a27f7e433633dfe81832a415162c8550f
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2023-11-23 16:05:37 +0000

    set up complete

---
+++ meme-gen/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header.js'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="main_container">
+        <Header />
+      </h1>
+    </>
+  )
+}
+
+export default App
---
+++ meme-gen/src/components/Header.js
@@ -0,0 +1,8 @@
+import { styles } from "./header.module.css"
+const Header = () => {
+  return (
+    <div className={styles.header_container}>Header</div>
+  )
+}
+
+export default Header

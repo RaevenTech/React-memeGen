720885e2fb288ff4df7c922718f7582ae11b87e4
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2023-11-24 17:38:00 +0000

    code refactor

--- meme-gen/src/components/Meme.jsx
+++ meme-gen/src/components/Meme.jsx
@@ -1,15 +1,23 @@
 import "./meme.css"
+import memsdata from "../../memsdata"
 
 const Meme = () => {
+
+  const getRandomImage = () => {
+    const randomIndex = Math.floor(Math.random() * memsdata.data.memes.length)
+    const randomMeme = memsdata.data.memes[randomIndex].url
+    console.log(randomMeme)
+    return randomMeme
+    // console.log(randomIndex)
+  }
+
   return (
     <main>
-      <form>
-        <div className="inputs">
-          <input className="txt_input" type="text" placeholder="Top text" />
-          <input className="txt_input" type="text" placeholder="bottom text" />
-        </div>
-        <button className="get_meme_btn">Get new meme image</button>
-      </form>
+      <div className="inputs">
+        <input className="txt_input" type="text" placeholder="Top text" />
+        <input className="txt_input" type="text" placeholder="bottom text" />
+      </div>
+      <button className="get_meme_btn" onClick={getRandomImage}>Get new meme image!</button>
     </main>
   )
 }
